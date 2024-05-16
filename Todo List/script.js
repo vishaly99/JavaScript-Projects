76dbfe8b552386fbba000e813ebe636667c5c003
vishaly99 <yennam20@gmail.com>
2024-05-16 16:02:33 +0530

    completed writing logic in script file

---
+++ Todo List/script.js
@@ -0,0 +1,32 @@
+const taskdata=[];
+function addtask(){
+    const task=document.getElementById('task').value;
+    if(!task){
+        alert("Please enter your task");
+    }else
+    {
+        taskdata.push(task);
+        console.log()
+        showdata();
+    }
+}
+function showdata(){
+    console.log(taskdata);
+
+    const todocontainerchild=document.getElementById('todo-container-parent');
+    todocontainerchild.innerHTML="";
+    taskdata.forEach((item,index)=>{
+        const value=item;
+        const data=` 
+        <span id="${item}">${item}</span> 
+        <i class="fa fa-trash-o" style="font-size:24px;color:white" onclick="deletetask(${index})"></i><br>`;
+        
+        todocontainerchild.innerHTML+=data;
+    });
+}
+
+function deletetask(item){
+    console.log(item)
+    delete taskdata[item];
+    showdata()
+}
